8c1012f5996c27095caf8ddbd45119d0bfea77d0
Greta Ramaneckaite <32294431+greta-ramaneckaite@users.noreply.github.com>
2023-10-08 13:55:17 +0200

    improve error handling and refactor lambda code

--- python/process-words/process-words.py
+++ python/process-words/process-words.py
@@ -8,38 +8,48 @@ dynamodb = boto3.resource("dynamodb")
 table = dynamodb.Table("word_map")
 
 
-def lambda_handler(event, context):
+class HttpErrorMessage:
+    HTTP_400_SENTENCE_PARAM_MISSING = (400, "Bad Request: Sentence param is missing in request body")
+    HTTP_400_SENTENCE_PARAM_WRONG_TYPE = (400, "Bad Request: Sentence param is not a string")
 
-    # Validate input sentence
+    @staticmethod
+    def log_and_fail(error_message):
+        error_message_log = f"Status code {error_message[0]} - {error_message[1]}"
+        logging.warning(error_message_log)
+        raise Exception(error_message_log)
+    
+
+def validate_input(event):
     if "sentence" not in event:
-        logging.warning('sentence param is missing in request body')
-        raise Exception('Status code 400 - Bad Request')
-
-    input_text = event['sentence']
-
-    # Define a regex pattern to match words
-    word_pattern = r'\b\w+\b'
-
-    # Process input sentence
-    response = {
-        "mapped_sentence": re.sub(word_pattern, replace_db_hit, input_text)
-    }
-
-    return response
+        HttpErrorMessage.log_and_fail(HttpErrorMessage.HTTP_400_SENTENCE_PARAM_MISSING)
+    elif not isinstance(event['sentence'], str):
+        HttpErrorMessage.log_and_fail(HttpErrorMessage.HTTP_400_SENTENCE_PARAM_WRONG_TYPE)
 
 
 def replace_db_hit(match):
     # Get word to match against database
     word = match.group(0)
-
+    # Query the database using the word as primary key
     fetched_object = table.get_item(
         Key={
             "word": word.lower()
         }
     )
-
     if "Item" in fetched_object:
+        # A mapping exists in the database, return it
         mapped_word = fetched_object['Item']['mapped_word']
     else:
+        # A mapping does not exist in the database, return the original word
         mapped_word = word
     return mapped_word
+
+
+def lambda_handler(event, context):
+    validate_input(event)
+    input_text = event['sentence']
+    # Regex pattern to match words
+    WORD_REGEX_PATTERN = r'\b\w+\b'
+    # Process input sentence
+    return {
+        "mapped_sentence": re.sub(WORD_REGEX_PATTERN, replace_db_hit, input_text)
+    }
